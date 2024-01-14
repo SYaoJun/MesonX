ce298f365065f055350d7e8b796fc3989165b4af
yaojun <940334249@qq.com>
2024-01-14 16:41:30 +0800

    add myself library

--- tests/add_test.cpp
+++ tests/add_test.cpp
@@ -1,8 +1,7 @@
 #include <gtest/gtest.h>
-#include "../math/add.h"  // 引入你要测试的源文件头文件
+#include "../math/add.h"  
 
 // 示例测试用例
 TEST(YourTestSuite, ExampleTest) {
-    // 通过 ASSERT 断言来检查测试条件
-    ASSERT_EQ(110, 110);
+    ASSERT_EQ(300, add(100, 200));
 }
